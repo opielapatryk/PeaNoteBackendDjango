466aca7c3fd4ddf1d2feced043521b77cab093c3
opielapatryk <xpatrykopiela@gmail.com>
2023-10-12 18:07:06 +0200

    setup done

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Backend -> Django, PostgreSQL
+# Frontend -> React Js
+
+## Main idea of this project is to create app where people will have possibility to pin tasks on other poeples boards.. 
--- stickify/settings.py
+++ stickify/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 import environ
+import os
 env = environ.Env()
 environ.Env.read_env()
 
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'stickify.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
