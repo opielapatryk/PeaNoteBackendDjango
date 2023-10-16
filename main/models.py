14183d58ebcb38bef247b89a40e8a076bc48b2a8
opielapatryk <xpatrykopiela@gmail.com>
2023-10-16 10:32:36 +0200

    models

--- main/models.py
+++ main/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class Color(models.Model):
-    value = models.CharField(max_length=6)
-
 class User(AbstractUser):
-    friends = models.ManyToManyField('self', symmetrical=True)
+    friends = models.ManyToManyField('self', symmetrical=True, blank=True)
     isPrivate = models.BooleanField(default=False)
     askBeforeStick = models.BooleanField(default=False)
-    boardColor = models.ForeignKey(Color, on_delete=models.CASCADE, default=None, null=True)
-    stickersOnBoard = models.ManyToManyField('Sticker')
+    stickersOnBoard = models.ManyToManyField('Sticker', blank=True)
 
 class Sticker(models.Model):
     content = models.TextField(max_length=150)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def settings(request):
+    pass
+def myboard(request):
+    pass
+def register(request):
+    pass
+def login(request):
+    pass
+def friend(request):
+    pass
+def friends(request):
+    pass
