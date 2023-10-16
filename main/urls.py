492c642639370fc82aa309e87c4d9830a91eced7
opielapatryk <xpatrykopiela@gmail.com>
2023-10-16 18:35:44 +0200

    templates

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path("settings", views.settings, name="settings"),
-    path("myboard", views.myboard, name="myboard"),
     path("register", views.register, name="register"),
     path("login", views.login, name="login"),
     path("friends/<int:user_id>", views.friend, name="friend"),
     path("friends", views.friends, name="friends"),
+    path("", views.index, name="index"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
-def settings(request):
-    pass
-def myboard(request):
-    pass
-def register(request):
-    pass
+# your board
+def index(request):
+    if request.user.is_authenticated:
+        return render(request, 'index.html', context={})
+    else:
+        return render(request, 'login.html', context={})
+
+# login page
 def login(request):
-    pass
+    return render(request, 'login.html', context={})
+
+# register page
+def register(request):
+    return render(request, 'register.html', context={})
+
+# settings
+def settings(request):
+    return render(request, 'settings.html', context={})
+
+# your friend board
 def friend(request):
-    pass
+    return render(request, 'friend.html', context={})
+
+# list your friends
 def friends(request):
-    pass
+    return render(request, 'friends.html', context={})
