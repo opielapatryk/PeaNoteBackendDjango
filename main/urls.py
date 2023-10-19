9ec7e92eff59014211c33c410a6bfea352a20672
opielapatryk <xpatrykopiela@gmail.com>
2023-10-19 11:50:00 +0200

    CBV

--- main/urls.py
+++ main/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("settings", views.settings, name="settings"),
+    path("settings", views.Settings.as_view(), name="settings"),
     path("register", views.Register.as_view(), name="register"),
     path("login", views.Login.as_view(), name="login"),
-    path("friends/<int:user_id>", views.friend, name="friend"),
-    path("friends", views.friends, name="friends"),
+    path("friends/<int:user_id>", views.Friend.as_view(), name="friend"),
+    path("friends", views.Friends.as_view(), name="friends"),
     path("", views.index, name="index"),
     path("logout", views.logout_view, name="logout"),
+    path('search', views.SearchFriend.as_view(), name="search")
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from main.models import User
 from .forms import LoginForm, RegisterForm
 from django.views import generic 
 from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
+from django.views.generic import ListView
+from django.views.generic import TemplateView
 
 def index(request):
     if request.user.is_authenticated:
@@ -25,11 +28,49 @@ class Login(LoginView):
     template_name = 'login.html'
     authentication_form = LoginForm
 
-def settings(request):
-    return render(request, 'settings.html', context={})
+class Settings(TemplateView):
+    template_name = 'settings.html'
 
-def friend(request):
-    return render(request, 'friend.html', context={})
+class Friend(TemplateView):
+    template_name = 'friend.html'
 
-def friends(request):
-    return render(request, 'friends.html', context={})
+class Friends(ListView):
+    template_name = 'friends.html'
+    model = User
+
+class SearchFriend(ListView):
+    template_name = 'search.html'
+    model = User
+
+    # def get_queryset(self):
+    #     queryset = self.model.objects.all()
+    #     search_query = self.request.GET.get('q')
+    #     if search_query:
+    #         queryset = queryset.filter(email__icontains=search_query)
+    #     return queryset
+
+    # Query for requested email
+    # try:
+    #     email = Email.objects.get(user=request.user, pk=email_id)
+    # except Email.DoesNotExist:
+    #     return JsonResponse({"error": "Email not found."}, status=404)
+
+    # # Return email contents
+    # if request.method == "GET":
+    #     return JsonResponse(email.serialize())
+
+    # # Update whether email is read or should be archived
+    # elif request.method == "PUT":
+    #     data = json.loads(request.body)
+    #     if data.get("read") is not None:
+    #         email.read = data["read"]
+    #     if data.get("archived") is not None:
+    #         email.archived = data["archived"]
+    #     email.save()
+    #     return HttpResponse(status=204)
+
+    # # Email must be via GET or PUT
+    # else:
+    #     return JsonResponse({
+    #         "error": "GET or PUT request required."
+    #     }, status=400)
