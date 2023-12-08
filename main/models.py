2749146fe6316f8c10a4f0c692d6fa665fafbb2d
opielapatryk <xpatrykopiela@gmail.com>
2023-12-08 14:10:39 +0100

    friends requests field

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     friends = models.ManyToManyField('self', symmetrical=True, blank=True)
+    friends_requests = models.ManyToManyField('self', symmetrical=True, blank=True)
     isPrivate = models.BooleanField(default=False)
     askBeforeStick = models.BooleanField(default=False)
     stickersOnBoard = models.ManyToManyField('Sticker', blank=True, related_name='stickers_on_board')
